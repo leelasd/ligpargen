d5a3b093cae484ecd56ed60e0d735d0f30db1665
Leela Dodda <leelasd@users.noreply.github.com>
2016-11-15 18:37:17 -0500

    Update LigParGenTools.py

--- LigParGenTools.py
+++ LigParGenTools.py
@@ -63,35 +63,45 @@ def read_pdb(fname):
     atoms = []
     coos = []
     pdb_lines = {}
+    cons=[]
     for line in fcon:
         if ('ATOM' in line) or ('HETATM' in line):
             atoms.append(line[12:16].strip())
             coos.append(list(map(float, line[32:54].split())))
+	if('CONECT' in line): cons.append(map(int,line.split()[1:]))
             # pdb_lines[line[12:16].strip()]=list(map(float,line[32:54].split()))
-    return atoms, coos
+    return atoms, coos, cons
 
 ########################## FOR READING BOXMAKER COOS ######################
 
 
-def pdb_lines(atoms, coos, ID, resid='UNK'):
+def pdb_lines(atoms, coos, ID, resid='UNK',offset=0):
     lines = []
-    num = (ID - 1) * len(atoms)
+    num = offset+1
     for (i, j) in zip(atoms, coos):
-        num += 1
         lines.append('%-6s%5d %4s %3s  %4d    %8.3f%8.3f%8.3f\n' %
                      ('ATOM', num, i, resid, ID, j[0], j[1], j[2]))
-    lines.append('TER \n')
+        num += 1
+    lines.append('TER\n')
+    return lines
+
+def con_lines(cons,offset):
+    lines = []
+    num = offset
+    for i in cons:
+        lines.append('%-6s%5d%5d\n' %
+                     ('CONECT',i[0]+num,i[1]+num))
     return lines
 
 ########################## FOR READING BOXMAKER COOS ######################
 
 
 def BOX_MAKER(pdb_file, BOX_SIZE):
-    atMOL, csMOL = read_pdb(pdb_file)
+    atMOL, csMOL,conMOL = read_pdb(pdb_file)
     csMOL, comMOL, radMOL = CenOfCoos(atMOL, csMOL)
-    maxX = 1.5 + (max([c[0] for c in csMOL]) - min([c[0] for c in csMOL]))
-    maxY = 1.5 + (max([c[1] for c in csMOL]) - min([c[1] for c in csMOL]))
-    maxZ = 1.5 + (max([c[2] for c in csMOL]) - min([c[2] for c in csMOL]))
+    maxX = radMOL*0.5 + (max([c[0] for c in csMOL]) - min([c[0] for c in csMOL]))
+    maxY = radMOL*0.5 + (max([c[1] for c in csMOL]) - min([c[1] for c in csMOL]))
+    maxZ = radMOL*0.5 + (max([c[2] for c in csMOL]) - min([c[2] for c in csMOL]))
     (nx, ny, nz) = (int(BOX_SIZE / maxX),
                     int(BOX_SIZE / maxY), int(BOX_SIZE / maxZ))
     gx = np.linspace(-0.5 * BOX_SIZE, 0.5 * BOX_SIZE, num=nx)
@@ -101,19 +111,65 @@ def BOX_MAKER(pdb_file, BOX_SIZE):
     BOX = {}
     ID = 0
     total_lines = []
+    tot_con_lines = []
     for XC in gx:
         for YC in gy:
             for ZC in gz:
                 csMOL = rotzyx(csMOL)
                 csF = trans(csMOL, [XC, YC, ZC])
                 BOX[ID] = {'ATS': atMOL, 'CS': csF}
-                total_lines += pdb_lines(BOX[ID]['ATS'], BOX[ID]['CS'], ID + 1)
+                total_lines += pdb_lines(BOX[ID]['ATS'], BOX[ID]['CS'], ID + 1,resid='OCT',offset=ID*len(atMOL))
+                tot_con_lines += con_lines(conMOL, offset=ID*len(atMOL))
                 ID = ID + 1
 
     total_mols = ID
     ofile = open('box_%d_' % (int(BOX_SIZE)) + pdb_file, 'w+')
-    for l in total_lines:
+    ofile.write('LIGPARGEN GENERATED CUSTOM SOLVENT BOX \n')
+    ofile.write("CRYST1 %8.3f %8.3f %8.3f  90.00  90.00  90.00 P 1           1\n"%(BOX_SIZE,BOX_SIZE,BOX_SIZE))
+    for l in total_lines +['TER \n'] +tot_con_lines:
         ofile.write('%s' % l)
+    ofile.write('END \n')
     ofile.close()
     return None
 ########################## FOR READING BOXMAKER COOS ######################
+def SOLVATE_SOLUTE(X_file,S_file, BOX_SIZE):
+    atMOL, csMOL,conMOL = read_pdb(X_file)
+    csMOL, comMOL, radMOL = CenOfCoos(atMOL, csMOL)
+    atS,     csS,  conS = read_pdb(S_file)
+    csS,    comS,  radS = CenOfCoos(atS, csS)
+    BOX_SIZE = BOX_SIZE+ radS
+    maxX = radMOL*0.5 + (max([c[0] for c in csMOL]) - min([c[0] for c in csMOL]))
+    maxY = radMOL*0.5 + (max([c[1] for c in csMOL]) - min([c[1] for c in csMOL]))
+    maxZ = radMOL*0.5 + (max([c[2] for c in csMOL]) - min([c[2] for c in csMOL]))
+    (nx, ny, nz) = (int(BOX_SIZE / maxX),
+                    int(BOX_SIZE / maxY), int(BOX_SIZE / maxZ))
+    gx = np.linspace(-0.5 * BOX_SIZE, 0.5 * BOX_SIZE, num=nx)
+    gy = np.linspace(-0.5 * BOX_SIZE, 0.5 * BOX_SIZE, num=ny)
+    gz = np.linspace(-0.5 * BOX_SIZE, 0.5 * BOX_SIZE, num=nz)
+    BOX = {}
+    BOX[0]={'ATS': atS, 'CS': csS}
+    ID = 1
+    total_lines = pdb_lines(BOX[0]['ATS'], BOX[0]['CS'], 1,resid='UNK') 
+    tot_con_lines= con_lines(conS,0)
+    for XC in gx:
+        for YC in gy:
+            for ZC in gz:
+		if sqrt(XC**2+YC**2+ZC**2) >= radS:
+                    csMOL = rotzyx(csMOL)
+                    csF = trans(csMOL, [XC, YC, ZC])
+                    BOX[ID] = {'ATS': atMOL, 'CS': csF}
+                    total_lines += pdb_lines(BOX[ID]['ATS'], BOX[ID]['CS'], ID + 1,resid='OCT',offset=len(atS)+(ID-1)*len(atMOL))
+                    tot_con_lines += con_lines(conMOL, offset=len(atS)+(ID-1)*len(atMOL))
+                    ID = ID + 1
+		else:
+		    continue
+    total_mols = ID
+#    ofile = open('box_%d_' % (int(BOX_SIZE)), 'w+')
+    ofile = open('III.pdb','w+')
+    ofile.write('LIGPARGEN GENERATED CUSTOM SOLVENT BOX \n')
+    ofile.write("CRYST1 %8.3f %8.3f %8.3f  90.00  90.00  90.00 P 1           1\n"%(BOX_SIZE,BOX_SIZE,BOX_SIZE))
+    for l in total_lines + ['TER \n']+ tot_con_lines:
+        ofile.write('%s' % l)
+    ofile.write('END \n')
+    ofile.close()
+    return None
